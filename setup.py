fc50a2f933cbbd0c7efac076fa49459f5adaf3b2
Xavier Duran <xavier.duran@gmail.com>
2019-12-19 13:00:43 +0100

    Package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="github-ci-status",
+    version="0.0.1",
+    author="Xavier Duran",
+    author_email='xavier.duran@pm.me',
+    description="GitHub Developer REST API v3 helpers",
+    long_description=long_description,
+    url="https://github.com/xdurana/github-ci-status",
+    license='GPL-3.0',
+    packages=setuptools.find_packages()
+)
